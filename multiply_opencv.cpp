902db92968f7adcc93b591f209ef5c2c7b65ad35
jyah <j.dong@utah.edu>
2020-03-20 20:45:36 -0600

    print out aruco patterns

--- multiply_opencv.cpp
+++ multiply_opencv.cpp
@@ -1,27 +1,29 @@
-#include <opencv2/opencv.hpp>
-#include <stdint.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/calib3d.hpp>
 
-using namespace cv;
+#include <sstream>
+#include <iostream>
+#include <fstream>
+
 using namespace std;
+using namespace cv;
+
+void createArucoMarkers(){
+	Mat outputMarker;
+	Ptr<aruco::Dictionary> markerDictionary = aruco::getPredefinedDictionary(aruco::DICT_4X4_50);
+
+	for(int i = 0;i<50;i++){
+		aruco::drawMarker(markerDictionary,i,500,outputMarker,1);
+		ostringstream convert;
+		string imageName = "aruco/4x4Marker_";
+		convert<<imageName << i << ".jpg";
+		imwrite(convert.str(),outputMarker);
+	}
+}
 
 int main(int argv, char** argc){
-	Mat frame;
-
-	//VideoCapture vid(0); // use webcam
-	VideoCapture vid("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov");
-
-	namedWindow("Webcam", WINDOW_AUTOSIZE);
-
-	int fps =(int)vid.get(CAP_PROP_FPS);
-	if(!vid.isOpened()){
-		return -1;
-	}
-
-	while(vid.read(frame)){
-		imshow("Webcam", frame);
-		if(waitKey(1000/fps)>=0){
-			break;
-		}
-	}
-	return 1;
+	createArucoMarkers();
 }
